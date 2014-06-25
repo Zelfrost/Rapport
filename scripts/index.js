207fd1e7923e24db450bbf48a39ae60c0232b113
Deconinck Damien <deconinck.damien@gmail.com>
2014-06-25 18:29:03 +0200

    Fix !

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
         $('#menu ul').append('<li></li>');
     });
 
+    $('.corps img').each(function() {
+        if ($(this).width() < $(this).parent().width()) {
+            $(this).css('margin-top', ($(this).parent().height()-$(this).height())/2);
+        } else {
+            $(this).css({'height':'100%', 'width':'auto'});
+        }
+    });
+
     var items = $('#menu li').click(function() {
         var index = items.index(this);
         $('html, body').animate({
