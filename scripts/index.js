bfa3b70d09491963eec076a9c4e78f5121c5b256
Deconinck Damien <deconinck.damien@gmail.com>
2014-06-25 15:25:20 +0200

    Delete all

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var items = $('#menu li').click(function() {
-        var index = items.index(this);
-        $('html, body').animate({
-            'scrollTop': index * $(window).height()
-        }, 700);
-    });
-});
