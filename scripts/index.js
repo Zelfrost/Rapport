646e17170b65365569f0f66ac74de926a777fbd4
Deconinck Damien <deconinck.damien@gmail.com>
2014-06-27 01:42:23 +0200

    Fix and add menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,17 @@
 $(window).bind('load', function() {
-    $(window).resize();
+    var pos = new Array(
+        0,
+        $(window).height(),
+        $(window).height()*2,
+        $(window).height()*3
+    );
+    var curr = 0;
+
+    if ($(window).width() > 1024) {
+        $('#html').data('scroll', 'off');
+    } else {
+        $('#html').data('scroll', 'on');
+    }
 
     $('.page').each(function() {
         $('#menu ul').append('<li></li>');
@@ -8,9 +20,9 @@ $(window).bind('load', function() {
     $('#menu li').animate({color:'#FFF'}, 600).animate({color:'#428BCA'}, 600);
 
     var items = $('#menu li').click(function() {
-        var index = items.index(this);
-        $('#html').animate({
-            'scrollTop': index * $(window).height()
+        curr = items.index(this);
+        $('#html').stop().animate({
+            'scrollTop': pos[curr]
         }, 700);
     });
 
@@ -22,10 +34,23 @@ $(window).bind('load', function() {
 
     $(window).resize(function() {
         textSize($(this));
-        if ($(window).width() > 1024) {
+        if ($(window).width() > 1024 && $('#html').data('scroll') == 'on') {
+            pos = new Array(
+                0,
+                $(window).height(),
+                $(window).height()*2,
+                $(window).height()*3
+            );
             bindHandler();
-        } else {
+        } else if ($(window).width() <= 1024 && $('#html').data('scroll') == 'off') {
             unbindHandler();
+        } else if ($('#html').data('scroll') == 'off') {
+            pos = new Array(
+                0,
+                $(window).height(),
+                $(window).height()*2,
+                $(window).height()*3
+            );
         }
     });
 
@@ -41,34 +66,59 @@ $(window).bind('load', function() {
             $('#html').unbind();
         }
     });
-});
 
-function textSize()
-{
-    $('.text-wrapper').each(function() {
-        var p = $(this).parent().parent();
-        if ($(this).height() > $(p).height()) {
-            $(p).css('height', $(this).height() + 60);
-        }
+    $('#info button').click(function() {
+        $('#info').remove();
     });
-}
 
-function bindHandler()
-{
-    $('.application .content').css('display', 'none');
-    $('.application .wrapper').hover(
-        function() {
-            $('.content', this).stop().slideDown();
-        },
-        function() {
-            $('.content', this).stop().slideUp();
-        }
-    );
+    function textSize()
+    {
+        $('.text-wrapper').each(function() {
+            var p = $(this).parent().parent();
+            if ($(this).height() > $(p).height()) {
+                $(p).css('height', $(this).height() + 60);
+            }
+        });
+    }
 
-}
+    function bindHandler()
+    {
+        $('.application .content').css('display', 'none');
+        $('.application .wrapper').hover(
+            function() {
+                $('.content', this).stop().slideDown();
+            },
+            function() {
+                $('.content', this).stop().slideUp();
+            }
+        );
 
-function unbindHandler()
-{
-    $('.application .content').css('display', 'block');
-    $('.application .wrapper').unbind();
-}
+        $(document).on('keyup', function(e) {
+            if (e.which == 38) {
+                if (curr > 0) {
+                    curr --;
+                }
+            } else if (e.which == 40) {
+                if (curr < 4) {
+                    curr ++;
+                }
+            }
+
+            $('#html').stop().animate({
+                'scrollTop': pos[curr]
+            }, 700);
+        });
+
+        $('#html').data('scroll', 'off');
+    }
+
+    function unbindHandler()
+    {
+        $('.application .content').css('display', 'block');
+
+        $('.application .wrapper').unbind();
+        $(document).unbind('keyup');
+
+        $('#html').data('scroll', 'on');
+    }
+});
